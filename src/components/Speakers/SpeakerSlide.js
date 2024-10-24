54e84282f549d81c0a38ed3d10a930fb3f5e256f
Arham अर्हम् <88226830+Vedarham@users.noreply.github.com>
2024-10-24 23:06:02 +0530

    upd6

--- src/components/Speakers/SpeakerSlide.js
+++ src/components/Speakers/SpeakerSlide.js
@@ -7,7 +7,6 @@ const SpeakerSlide = () => {
     { name: "Shishir Somani", role: "CEO Archer Communications", src: require("./assets/images/Shishir-Somani.JPG") },
     { name: "Sufiyan Rao", role: "CEO & Founder Brain Inventory", src: require("./assets/images/Sufiyan-Rao.jpeg") },
     { name: "Ujjawal Pahwa", role: "Founder Cosmic Club", src: require("./assets/images/Ujjawal-Pahwa.jpg") },
-    { name: "Vibha Vyas", role: "Author - Billionaire's Hired Wife", src: require("./assets/images/Vibha-Vyas.png") },
     { name: "Anmol Gupta", role: "CEO 7Prosper", src: require("./assets/images/Anmol-Gupta.jpeg") },
     { name: "Ashutosh Gupta", role: "CMO THOUGHTWIN", src: require("./assets/images/Ashutosh-Gupta.jpeg") },
     { name: "Chetna Joshi Tiwari", role: "Miss India World 2023", src: require("./assets/images/Chetna-Joshi-Tiwari.jpeg") },
@@ -17,7 +16,7 @@ const SpeakerSlide = () => {
     { name: "Munish Jindal", role: "Founder & CEO of HoverRobotix", src: require("./assets/images/Munish-Jindal.jpeg") },
     { name: "Prashant Kulkarni", role: "Founder Tasty Alphabets Pvt. Ltd.", src: require("./assets/images/prashant-Kulkarni.jpeg") },
     { name: "Pushpendra Singh Jadon", role: "Founder & Director Key Elements", src: require("./assets/images/Pushpendra-Singh-jadon.jpg") },
-
+    { name: "Vibha Vyas", role: "Author - Billionaire's Hired Wife", src: require("./assets/images/Vibha-Vyas.png") },
   ];
 
   return (
